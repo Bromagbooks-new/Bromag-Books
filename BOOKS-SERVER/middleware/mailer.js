2568543f5c4a96ac1e374fb786ae3f3fe802ba3d
BROMAG BOOK <bromagbook@gmail.com>
2024-06-23 15:30:19 +0530

    Update mailer.js

--- BOOKS-SERVER/middleware/mailer.js
+++ BOOKS-SERVER/middleware/mailer.js
@@ -2,7 +2,8 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
-  port: 465,
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASS,

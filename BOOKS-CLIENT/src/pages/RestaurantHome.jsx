3519686cd15e451ad3a54d31199e888354e863c7
Mohd Hussain <md28md.hussain@gmail.com>
2024-10-29 01:51:13 +0530

    changes bg on restro login

--- BOOKS-CLIENT/src/pages/RestaurantHome.jsx
+++ BOOKS-CLIENT/src/pages/RestaurantHome.jsx
@@ -13,6 +13,10 @@ import { Separator } from "@/components/ui/separator";
 import { Button } from "@/components/ui/button";
 import { employeeLogin } from "../config/routeApi/restaurant";
 import { adminLogin } from "../config/routeApi/restaurant";
+import ellipse545 from '@/assets/images/landing-images/Ellipse 545.svg'
+import ellipse544 from '@/assets/images/landing-images/Ellipse 544.svg'
+import group212 from '@/assets/images/landing-images/Group 212.svg'
+import subtract from '@/assets/images/landing-images/Subtract.svg';
 
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
@@ -147,13 +151,22 @@ const RestaurantHome = () => {
 
   return (
     <div className="w-screen h-screen flex md:block flex-col items-center justify-center bg-[#1F303C]">
-      <div className="md:w-3/5 md:h-full px-12 py-16 rounded-3xl md:rounded-none md:p-0 md:rounded-r-[4rem] bg-white">
+      <div className="relative h-0 ">
+        <img className="relative z-10 w-[23rem] md:w-auto top-[1rem] left-[8%] md:left-[20%] opacity-50" src={subtract} />
+      </div>
+      <div className="relative h-0">
+        <img className="relative z-0 w-[9rem]  md:w-auto left-[70%] md:left-[100%] top-[10rem] md:top-10" src={ellipse545} />
+      </div>
+      <div className="relative h-0">
+        <img className="relative z-10 w-[20rem] md:w-auto right-[11rem] md:right-[33rem] top-32 md:top-20" src={ellipse544} />
+      </div>
+      <div className="md:w-3/5 md:h-full px-12 py-16 rounded-3xl md:rounded-none md:p-0 md:rounded-r-[4rem] ">
         <div className="flex items-center justify-center h-full text-white font-roboto z-40">
           <form
             onSubmit={handleSubmit(handleLoginSubmit)}
             className="md:w-2/4 flex flex-col gap-4 z-40"
           >
-            <p className=" text-black text-4xl mb-10 -mt-10 font-semibold text-center">
+            <p className=" text-white text-4xl mb-10 -mt-10 font-semibold text-center">
               WELCOME TO BROMAG BOOKS
             </p>
             <p className=" text-gray-500 text-3xl font-semibold text-center">
@@ -168,11 +181,11 @@ const RestaurantHome = () => {
               className="flex justify-center gap-2 text-black"
               onValueChange={setSelectedRole}
             >
-              <div className="flex items-center space-x-2 rounded-xl border-2 p-3">
+              <div className="flex items-center text-white space-x-2 rounded-xl border-2 p-3">
                 <RadioGroupItem value="admin" id="option-one" />
                 <Label htmlFor="option-one">Admin Login</Label>
               </div>
-              <div className="flex items-center space-x-2 rounded-xl border-2 p-3">
+              <div className="flex items-center text-white space-x-2 rounded-xl border-2 p-3">
                 <RadioGroupItem value="employee" id="option-two" />
                 <Label htmlFor="option-two">Employee Login</Label>
               </div>
@@ -211,7 +224,7 @@ const RestaurantHome = () => {
             <div className="flex justify-center">
               <Button
                 type="submit"
-                className="bg-[#20303C]  w-2/3 rounded-lg p-3 text-xl uppercase text-white"
+                className="bg-[#56DDE8]  w-2/3 rounded-lg p-3 text-xl uppercase text-black"
               >
                 Login
               </Button>

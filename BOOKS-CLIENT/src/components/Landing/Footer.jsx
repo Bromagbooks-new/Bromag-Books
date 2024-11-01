01e94fc2a89417701111d11d870539c50b31033a
Mohd Hussain <md28md.hussain@gmail.com>
2024-11-01 10:49:57 +0530

    add the social media link to the footer

--- BOOKS-CLIENT/src/components/Landing/Footer.jsx
+++ BOOKS-CLIENT/src/components/Landing/Footer.jsx
@@ -12,7 +12,7 @@ import {
 import { BsWhatsapp } from "react-icons/bs";
 
 import ellipse546 from "@/assets/images/landing-images/Ellipse 546.svg";
-import ellipse544 from '@/assets/images/landing-images/Ellipse 544.svg'
+import ellipse544 from "@/assets/images/landing-images/Ellipse 544.svg";
 import group216 from "@/assets/images/landing-images/Group 216.svg";
 import { Link } from "react-router-dom";
 
@@ -24,10 +24,10 @@ const Footer = () => {
     >
       <div className="flex flex-col lg:flex-row md:pl-32 items-center justify-center gap-16 md:gap-32 w-full">
         <div className="flex flex-col md:flex-row items-center">
-          <img src={logo} className=" w-48 h-48 z-10" />
+          <img src={logo} className="w-48 h-48 z-10" alt="Bromag Books Logo" />
           <p className="block text-xl z-20 md:hidden w-4/5 text-center text-gray-500">
-            A platform that provides a comprehensive accounting system for
-            businesses that are growing
+            A platform that provides a comprehensive accounting system for
+            businesses that are growing.
           </p>
         </div>
         <div className="flex flex-col gap-4 items-center md:items-start">
@@ -35,15 +35,15 @@ const Footer = () => {
           <ul className="text-gray-500 flex flex-col items-center md:items-start gap-2 text-xl">
             <li className="flex gap-2">
               <Phone />
-              <p className="">+91 9150289762</p>
+              <p>+91 9150289762</p>
             </li>
             <li className="flex gap-2">
               <Mail />
-              <p className="">bromag0507@gmail.com</p>
+              <p>bromag0507@gmail.com</p>
             </li>
             <li className="flex gap-2">
               <MapPin />
-              <p className="">Chennai, Tamil Nadu</p>
+              <p>Chennai, Tamil Nadu</p>
             </li>
           </ul>
         </div>
@@ -59,18 +59,38 @@ const Footer = () => {
           <p className="text-3xl text-white font-bold">Connect Us</p>
 
           <div className="flex gap-4 md:justify-end pt-4">
-            <div className="rounded-full border-2 border-gray-400  text-gray-500 flex items-center justify-center p-2">
+            <a
+              href="https://www.instagram.com/bromagbooks?igsh=eW5ieXF2NDY0YXdt"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="rounded-full border-2 border-gray-400 text-gray-500 flex items-center justify-center p-2"
+            >
               <Instagram className="w-8 h-8" />
-            </div>
-            <div className="rounded-full border-2 border-gray-400  text-gray-500 flex items-center justify-center p-2">
+            </a>
+            <a
+              href="https://www.youtube.com/@BROMAGINDIA"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="rounded-full border-2 border-gray-400 text-gray-500 flex items-center justify-center p-2"
+            >
               <Youtube className="w-8 h-8" />
-            </div>
-            <div className="rounded-full border-2 border-gray-400  text-gray-500 flex items-center justify-center p-2">
+            </a>
+            <a
+              href="https://wa.me/message/AUK3D7HGZCVED1"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="rounded-full border-2 border-gray-400 text-gray-500 flex items-center justify-center p-2"
+            >
               <BsWhatsapp className="w-8 h-8" />
-            </div>
-            <div className="rounded-full border-2 border-gray-400  text-gray-500 flex items-center justify-center p-2">
+            </a>
+            <a
+              href="https://www.linkedin.com/company/bromagbooks/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="rounded-full border-2 border-gray-400 text-gray-500 flex items-center justify-center p-2"
+            >
               <Linkedin className="w-8 h-8" />
-            </div>
+            </a>
           </div>
         </div>
       </div>
@@ -79,20 +99,23 @@ const Footer = () => {
       </p>
       <div className="relative h-0 hidden md:block">
         <img
-          className="relative z-0 h-[22rem] opacity-60 bottom-[19rem] right-[110%] "
+          className="relative z-0 h-[22rem] opacity-60 bottom-[19rem] right-[110%]"
           src={ellipse546}
+          alt="Decorative Ellipse"
         />
       </div>
       <div className="relative h-0 block md:hidden">
         <img
-          className="relative z-0 h-[20rem]  bottom-[70rem] right-[90%] "
+          className="relative z-0 h-[20rem] bottom-[70rem] right-[90%]"
           src={ellipse544}
+          alt="Decorative Ellipse"
         />
       </div>
       <div className="relative h-0">
         <img
-          className="relative z-0 h-[20rem] md:h-[28rem] opacity-60 bottom-[23.5rem] rotate-90 md:rotate-0 transform -scale-y-100 md:scale-y-100 md:bottom-[28rem] left-[11%] md:left-[55%] "
+          className="relative z-0 h-[20rem] md:h-[28rem] opacity-60 bottom-[23.5rem] rotate-90 md:rotate-0 transform -scale-y-100 md:scale-y-100 md:bottom-[28rem] left-[11%] md:left-[55%]"
           src={group216}
+          alt="Decorative Group"
         />
       </div>
     </footer>
